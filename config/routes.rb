5db7721c957cb787dcfd145fe4d5840cec8143a8
megan <megan.schertz@gmail.com>
2021-06-01 14:12:33 -0500

    create varietals

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :varietals
+  resources :types
+  resources :grapes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20210601191127_create_varietals.rb
@@ -0,0 +1,10 @@
+class CreateVarietals < ActiveRecord::Migration[6.1]
+  def change
+    create_table :varietals do |t|
+      t.string :name
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
